9abbc0e3895a531fad8093d3e9745d1685906394
Steven Rychlik <steven@rychlik.family>
2025-12-01 23:44:55 -0600

    Day 1 - Part 1

--- days/day-1.lisp
+++ days/day-1.lisp
@@ -16,17 +16,51 @@
 
 ;;; The safe has a dial with only an arrow on it; around the dial are the numbers 0 through 99 in order. As you turn the dial, it makes a small click noise as it reaches each number.
 
+(defparameter *min-dial* 0)
+(defparameter *max-dial* 99)
+
 ;;; The attached document (your puzzle input) contains a sequence of rotations, one per line, which tell you how to open the safe. A rotation starts with an L or R which indicates whether the rotation should be to the left (toward lower numbers) or to the right (toward higher numbers). Then, the rotation has a distance value which indicates how many clicks the dial should be rotated in that direction.
 
+(defun rotate-dial (start direction distance)
+  "Rotate the dial of the lock <direction L or R> <distance spaces> starting at <start>. Return the new value"
+  (cond ((equal direction "R") (mod (+ start distance) (+ 1 *max-dial*)))
+	((equal direction "L") (mod (- start distance) (+ 1 *max-dial*)))
+	(t (error "Invalid direction"))
+	)
+  )
+  
 ;;; So, if the dial were pointing at 11, a rotation of R8 would cause the dial to point at 19. After that, a rotation of L19 would cause it to point at 0.
+
+(assert (equal (rotate-dial 11 "R" 8) 19))
+(assert (equal (rotate-dial 19 "L" 19) 0))
 
 ;;; Because the dial is a circle, turning the dial left from 0 one click makes it point at 99. Similarly, turning the dial right from 99 one click makes it point at 0.
 
 ;;; So, if the dial were pointing at 5, a rotation of L10 would cause it to point at 95. After that, a rotation of R5 could cause it to point at 0.
 
+(assert (equal (rotate-dial 5 "L" 10) 95))
+(assert (equal (rotate-dial 95 "R" 5) 0))
+
 ;;; The dial starts by pointing at 50.
 
+(defparameter *start-dial* 50)
+
 ;;; You could follow the instructions, but your recent required official North Pole secret entrance security training seminar taught you that the safe is actually a decoy. The actual password is the number of times the dial is left pointing at 0 after any rotation in the sequence.
+
+(defun parse-rotation (string)
+  ;; Not happy that this is a list, but multiple value bind doesn't work in a loop easily
+  ;; and I don't care to make it fancier. A list of 2 values is fine for direction and distance
+  (list (subseq string 0 1) (parse-integer (subseq string 1))))
+    
+(defun get-password (filename)
+  (with-open-file (stream filename)
+    (loop for line = (read-line stream nil)
+	  while line
+	  for firstp = t then nil
+	  for instruction-parts = (parse-rotation line)
+	  for dial = (if firstp (rotate-dial *start-dial* (first instruction-parts) (second instruction-parts)) (rotate-dial dial (first instruction-parts) (second instruction-parts)))
+	  count (= 0 dial))
+    ))
 
 ;;; For example, suppose the attached document contained the following rotations:
 
